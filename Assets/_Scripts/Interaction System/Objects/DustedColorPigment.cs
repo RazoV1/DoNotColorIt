f739564f73a207dc3df729a4578767ab7f595a7b
Tim16763 <94013118+RazoV1@users.noreply.github.com>
2026-01-30 01:04:19 +0300

    Сохранение перемолотого пигмента

--- Assets/_Scripts/Interaction System/Objects/DustedColorPigment.cs
+++ Assets/_Scripts/Interaction System/Objects/DustedColorPigment.cs
@@ -1,4 +1,7 @@
+using Assets._Scripts.Utils;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DustedColorPigment : ColorPigment
 {
@@ -10,5 +13,24 @@ public class DustedColorPigment : ColorPigment
 	public override void InitializePigment(Color color, float volume)
 	{
 		base.InitializePigment(color, volume);
+	}
+
+	public override void SaveData()
+	{
+		SavablePrefab pref = new SavablePrefab
+		{
+			prefabName = $"dust",
+			dimension = SceneManager.GetActiveScene().buildIndex,
+			worldPosition = Mapper.VectorToFloatData(transform.position),// new List<float> { transform.position.x, transform.position.y, transform.position.z },
+			quaternionRotation = Mapper.QuaternionToFloatData(transform.rotation), //new List<float> { transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w },
+			floatData = new Dictionary<string, float>
+			{
+				{"colorR",color.r},
+				{"colorG", color.g},
+				{"colorB",color.b},
+				{"volume",volume}
+			}
+		};
+		SaveManager.Instance.SavePrefab(pref);
 	}
 }
