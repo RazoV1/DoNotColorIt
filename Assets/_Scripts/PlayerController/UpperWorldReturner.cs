8e2f1987cc23a51a5538d2ffc999e6000ec19e3f
Tim16763 <94013118+RazoV1@users.noreply.github.com>
2026-01-30 01:07:55 +0300

    Сейв вылета за мапу

---
+++ Assets/_Scripts/PlayerController/UpperWorldReturner.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class UpperWorldReturner : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.tag == "Player")
+		{
+			player.transform.Translate(Vector3.up * 200);
+		}
+	}
+}
