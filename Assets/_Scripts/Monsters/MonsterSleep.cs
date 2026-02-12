93c9c07eedd445f26cdcb6188218ed9e0e72b54e
Tim16763 <94013118+RazoV1@users.noreply.github.com>
2026-02-12 18:05:31 +0300

    Ходьба (неоттесчено)

---
+++ Assets/_Scripts/Monsters/MonsterSleep.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts.Monsters
+{
+	public class MonsterSleep
+	{
+		[Header("Settings")]
+		[SerializeField] private float sleepStrenghtPerSec;
+		[Header("VFX")]
+		[SerializeField] private ParticleSystem sleepParticles;
+
+		private Animator animator;
+		private PigmentMonster monster;
+	    
+		public IEnumerator Sleep()
+		{
+			float strenght = monster.GetStrenght();
+			sleepParticles.Play();
+			//animator.SetBool("Sleeping",true);
+			while (strenght != 1)
+			{
+				float deltaStrenght = sleepStrenghtPerSec * Time.deltaTime;
+				monster.AddStenght(deltaStrenght);
+				strenght += deltaStrenght;
+				yield return null;
+			}
+			sleepParticles.Stop();
+			//animator.SetBool("Sleeping", false);
+		}
+	}
+}
--- Assets/_Scripts/Monsters/MonsterWalk.cs
+++ Assets/_Scripts/Monsters/MonsterWalk.cs
@@ -1,6 +1,8 @@
 using Assets._Scripts.Interaction_System.Objects;
+using Assets._Scripts.Monsters;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -9,6 +11,10 @@ public class MonsterWalk : MonoBehaviour
 {
 	[Header("Settings")]
 	[SerializeField] private float pointPickingRange;
+	[Header("Weights")]
+	[SerializeField] private float monsterWeight;
+	[SerializeField] private float wanderingWeight;
+	private MonsterSleep sleep;
 	private BasicItem basicItem;
 	private NavMeshAgent agent;
 	private PigmentMonster monster;
@@ -18,6 +24,7 @@ public class MonsterWalk : MonoBehaviour
 	private Fence home;
 	private Animator animator;
 	private Dictionary<string, float> stats;
+	private bool isWandering = false;
 
 	private void Start()
 	{
@@ -27,14 +34,23 @@ public class MonsterWalk : MonoBehaviour
 		rb = GetComponent<Rigidbody>();
 		baseMesh = GameObject.Find("ostrov_lp");
 		animator = GetComponentInChildren<Animator>();
-		TrySetupTarget();
+		sleep = GetComponent<MonsterSleep>();
+		TrySetupWanderingTarget();
 	}
 
 	private IEnumerator WanderCycle()
 	{
 		while (true)
 		{
-			stats = monster.GetMonsterStats();
+			DesideBehaviour();
+			while (agent.destination != null && Vector3.Distance(agent.destination, transform.position) > 1f)
+			{
+				yield return null;
+			}
+			if (!isWandering)
+			{
+				yield return sleep.Sleep();
+			}
 		}
 	}
 
@@ -43,24 +59,77 @@ public class MonsterWalk : MonoBehaviour
 		home = fence;
 	}
 
+	private void GoHome()
+	{
+		agent.SetDestination(ProjectTarget(home.transform.position));
+		agent.isStopped = false;
+	}
+
 	private void Interrupt()
 	{
 		if (basicItem.GetIsGrabbed())
 		{
 			agent.enabled = false;
-		    
 		}
 	}
 
-	private void TrySetupTarget()
+	private void DesideBehaviour()
+	{
+		stats = monster.GetMonsterStats();
+		float strenght = stats["strenght"];
+
+		float homeWeight = (strenght > 0.2f ? 0 : 1);
+		float monsterWeight = this.monsterWeight * strenght * Random.Range(0.8f,1.1f);
+		//Добавить реализацию POI сюда
+		float wanderWeight = this.wanderingWeight * strenght * Random.Range(0.8f,1.1f);
+
+		float maxWeight = Mathf.Max(homeWeight,monsterWeight, wanderWeight);
+
+		
+		if (maxWeight == wanderWeight)
+		{
+			TrySetupWanderingTarget();
+			isWandering = true;
+			return;
+		}
+		else if (maxWeight == homeWeight)
+		{
+			GoHome();
+			isWandering = false;
+			return;
+		}
+		else if (maxWeight == monsterWeight)
+		{
+			GoToMonster();
+			isWandering = true;
+			return;
+		}
+	}
+
+	private void GoToMonster()
+	{
+		List<PigmentMonster> monsters = FindAllMonsters();
+		Vector3 worldMonsterPos = monsters[Random.Range(0,monsters.Count)].transform.position;
+		agent.SetDestination(ProjectTarget(worldMonsterPos));
+	}
+
+	private List<PigmentMonster> FindAllMonsters() => GameObject.FindObjectsByType<PigmentMonster>(FindObjectsSortMode.InstanceID).Where(x => x != monster).ToList();
+	
+
+	private void TrySetupWanderingTarget()
 	{
 		pointInPlane = (Vector3)(Random.insideUnitCircle * pointPickingRange) + transform.position;
 		Debug.Log($"<color=green>Picking point in circle: {pointInPlane}");
-		NavMeshHit hit;
-		NavMesh.SamplePosition(pointInPlane, out hit, pointPickingRange * 2, NavMesh.AllAreas);
-		agent.SetDestination(hit.position);
+		agent.SetDestination(ProjectTarget(pointInPlane));
 		Debug.Log(agent.destination == null);
 		animator.SetBool("Walking", true);
+	}
+
+	private Vector3 ProjectTarget(Vector3 target)
+	{
+		NavMeshHit hit;
+		NavMesh.SamplePosition(pointInPlane, out hit, pointPickingRange * 2, NavMesh.AllAreas);
+		return hit.position;
 	}
 
 	private void Update()
@@ -79,7 +148,10 @@ public class MonsterWalk : MonoBehaviour
 		{
 			agent.enabled = true;
 			rb.isKinematic = true;
-			TrySetupTarget();
+			if (isWandering)
+			{
+				TrySetupWanderingTarget();
+			}
 		}
 	}
 
