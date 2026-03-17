11c35e074d2e29ff32a0b696e27b382bf6f187db
Tim16763 <94013118+RazoV1@users.noreply.github.com>
2026-03-17 22:48:47 +0300

    Подсказка машины, лок вожления

--- Assets/_Scripts/PlayerController/PlayerGrabber.cs
+++ Assets/_Scripts/PlayerController/PlayerGrabber.cs
@@ -101,7 +101,7 @@ public class PlayerGrabber : MonoBehaviour
 				{
 					GameManager.Instance.GetCursorHint().ShowHint(MouseHints.GetIn);
 				}
-				else if (cameraController.GetShouldRotate() && hit.collider.tag == "Portal" && GameManager.Instance.GetTutorial().GetTutorialIndex() >= 10)
+				else if (cameraController.GetShouldRotate() && hit.collider.tag == "Portal" && GameManager.Instance.GetTutorial().GetTutorialIndex() >= 5)
 				{
 					GameManager.Instance.GetCursorHint().ShowHint(MouseHints.EnterPocket);
 				}
@@ -313,7 +313,7 @@ public class PlayerGrabber : MonoBehaviour
 		{
 			if (!interactableTags.Contains(hit.collider.tag) && Vector3.Distance(cameraPivotTransform.position, hit.point) < maxGrabDistance)
 			{
-				if (cameraController.GetShouldRotate() && hit.collider.tag == "Fiat")
+				if (cameraController.GetShouldRotate() && hit.collider.tag == "Fiat" && GameManager.Instance.GetTutorial().GetTutorialIndex() >= 18)
 				{
 					cameraController.MountFiat(hit.collider.GetComponentInParent<MetlaController>());
 				}
