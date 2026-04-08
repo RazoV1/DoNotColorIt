5735067b165ea79920e09adbdd80909d0313a7ae
Tim16763 <94013118+RazoV1@users.noreply.github.com>
2026-04-08 16:54:19 +0300

    fix: follow-up to previous commit
    -Забыл залить скрипт

---
+++ Assets/_Scripts/PlayerController/CameraRaycaster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts.PlayerController
+{
+	public class CameraRaycaster : MonoBehaviour
+	{
+		[Header("Ray Settings")]
+		[SerializeField] private float maxDistance;
+		private RaycastHit hit;
+		private bool hasHitSomething;
+
+		private Camera camera;
+
+		private void Awake()
+		{
+			camera = Camera.main;
+		}
+
+		private void CastRay()
+		{
+			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+			hasHitSomething = Physics.Raycast(ray, out hit) && Vector3.Distance(hit.point, ray.origin) <= maxDistance;
+		}
+
+		public bool HasHitSomething() => hasHitSomething;
+
+		public RaycastHit GetHit() => hit;
+
+		private void Update()
+		{
+			CastRay();
+		}
+	}
+}
